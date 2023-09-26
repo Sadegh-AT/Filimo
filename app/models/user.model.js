e538e52ce668506a4d1bd855677a9b6dda87e6a8
SadeghAT <sadegh.artivox@gmail.com>
2023-09-27 00:20:51 +0330

    create User model schema

---
+++ app/models/user.model.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String },
+    last_name: { type: String },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, unique: true },
+    isSubscription: { type: Boolean, required: true, default: false },
+    roles: { type: Array, default: ["USER"] },
+    password: { type: String, required: true },
+    watched_movie: { type: Array, default: [] },
+    liked_movie: { type: Array, default: [] },
+    comments: { type: Array, default: [] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  UserModel,
+};
