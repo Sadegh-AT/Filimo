78e3abff57a3ff4c63bcd4422787bb1defd2c887
SadeghAT <sadegh.artivox@gmail.com>
2023-09-27 16:35:23 +0330

    add new thing

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,7 +3,7 @@ const { UserModel } = require("../models/user.model");
 async function getAllUser(req, res, next) {
   try {
     const result = await UserModel.find({}).limit(10).sort({ createdAt: -1 });
-
+    console.log("New Req: " + req.connection.remoteAddress);
     res.json(result);
   } catch (error) {
     next(error);
