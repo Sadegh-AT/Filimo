a58c38a41e871e7266973bf9bdc664a042854621
SadeghAT <sadegh.artivox@gmail.com>
2023-09-28 23:54:26 +0330

    generate token when user login

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const { UserModel } = require("../models/user.model");
 const { hashPassword, comparePassword } = require("../utils/hash-password");
 const PersianDate = require("../utils/persianDate");
+const { signToken } = require("../utils/token-generator");
 
 async function register(req, res, next) {
   try {
@@ -27,6 +28,18 @@ async function login(req, res, next) {
     if (!user) throw { status: 401, message: "phone or password is incorrect" };
 
     if (comparePassword(password, user.password)) {
+      const token = signToken({
+        id: user._id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        isSubscription: user.isSubscription,
+        loginTime: new PersianDate().now(),
+      });
+      console.log(token);
+      const tokenStratgy = `Bearer ${token}`;
+
+      res.cookie("jwtToken", tokenStratgy, { maxAge: 900000, httpOnly: true });
       res.send({ message: "login successfully" });
     }
   } catch (error) {
---
+++ app/utils/token-generator.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.SECRET;
+console.log(secret);
+function signToken(payload) {
+  return jwt.sign(payload, secret, { expiresIn: "20s" });
+}
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    throw {
+      status: 401,
+      message: "token is invalid, please login",
+    };
+  }
+}
+module.exports = {
+  signToken,
+  verifyToken,
+};
