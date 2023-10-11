f41fdf4e2372be14603610da93de3803b89cd68a
SadeghAT <sadegh.artivox@gmail.com>
2023-10-11 16:33:16 -0700

    create comment route

--- app/controllers/comment.controller.js
+++ app/controllers/comment.controller.js
@@ -1,14 +1,20 @@
 const CommentModel = require("../models/comment.model");
+const PersianDate = require("../utils/persianDate");
 
 async function createComment(req, res, next) {
   const { text } = req.body;
-  const { userId } = req.user;
-  const { movieId } = req.movie;
+  const { _id, first_name, last_name } = req.user;
+
+  // const { movieId } = req.movie;
   const comment = {
+    userId: _id,
+    fullName: `${first_name} ${last_name}`,
     text,
+    date: new PersianDate().now(),
+    movieId: null,
   };
-  await CommentModel.create();
-  res.send("");
+  await CommentModel.create(comment);
+  res.send("Comment Created");
 }
 async function getAllComment(req, res, next) {}
 async function searchComment(req, res, next) {}
