425b07db6f16f4f0443d3de653b1ec3ef77a4c93
SadeghAT <sadegh.artivox@gmail.com>
2023-10-15 01:04:00 -0700

    users can delete just own comment

--- app/middleware/checkAdminAccess.js
+++ app/middleware/checkAdminAccess.js
@@ -2,9 +2,14 @@ const createError = require("http-errors");
 async function checkAdminAccess(req, res, next) {
   try {
     const { roles } = req.user;
-    if (!roles.includes("ADMIN"))
-      throw createError.Forbidden("You are not admin!");
-    next();
+    if (!roles.includes("ADMIN")) {
+      req.adminAccess = false;
+      next();
+    } else {
+      req.adminAccess = true;
+
+      next();
+    }
   } catch (error) {
     next(error);
   }
--- app/routers/comment.routes.js
+++ app/routers/comment.routes.js
@@ -6,13 +6,21 @@ const {
   getUserComments,
 } = require("../controllers/comment.controller");
 const { checkAdminAccess } = require("../middleware/checkAdminAccess");
+const {
+  checkUserCanDeleteComment,
+} = require("../middleware/checkUserCanDeleteComment");
 const router = require("express").Router();
 
 router.get("/", getAllComment);
 router.get("/search", checkAdminAccess, searchComment);
 router.get("/user-comment", getUserComments);
 router.post("/create", createComment);
-router.delete("/delete/:id", checkAdminAccess, deleteCommentById);
+router.delete(
+  "/delete/:id",
+  checkAdminAccess,
+  checkUserCanDeleteComment,
+  deleteCommentById
+);
 module.exports = {
   commentRoutes: router,
 };
