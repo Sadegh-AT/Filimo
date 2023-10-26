254cc079c6b61d9729c3e008ff82348cdf400b1e
SadeghAT <sadegh.artivox@gmail.com>
2023-10-26 02:12:07 -0700

    args graphql

--- app/graphql/queries/user.resolver.js
+++ app/graphql/queries/user.resolver.js
@@ -1,11 +1,19 @@
-const { GraphQLList } = require("graphql");
+const { GraphQLList, GraphQLString } = require("graphql");
 const { UserModel } = require("../../models/user.model");
 const { UserType } = require("../typeDefs/user.type");
-
+const mongoose = require("mongoose");
 const UserResolver = {
   type: new GraphQLList(UserType),
-  resolve: async () => {
-    return await UserModel.find({}).populate("comments");
+  args: {
+    id: { type: GraphQLString },
+  },
+  resolve: async (_, args) => {
+    console.log(args);
+    if (Object.keys(args) <= 0)
+      return await UserModel.find({}).populate("comments");
+    return await UserModel.find({
+      _id: mongoose.Types.ObjectId(args),
+    }).populate("comments");
   },
 };
 module.exports = {
